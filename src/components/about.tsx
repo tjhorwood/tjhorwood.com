1054eddebafe550c12cf52d6d8d35a70b123ba37
Taylor Horwood <taylor.horwood@outlook.com>
2023-08-17 19:43:02 +0000

    updated structure

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -19,13 +19,7 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p>
-        I am a loving husband and father who enjoys spending time with my
-        family, engaging in outdoor activities, working on cars, and
-        creating/building things in my workshop. I am also passionate about
-        technology and enjoy tinkering with my homelab and coding for fun. These
-        hobbies have taught me valuable problem-solving skills and a desire to
-        constantly learn and improve. As a well-rounded individual, I bring a
-        unique perspective and a can-do attitude to every task.
+        Loving husband and father who enjoys spending time with my family, engaging in outdoor activities, working on cars, and creating/building things in my workshop. I am also very passionate about technology and enjoy tinkering with my homelab and coding for fun. These hobbies have taught me valuable problem-solving skills and a desire to constantly learn and improve. As a well-rounded individual, I bring a unique perspective and a can-do attitude to every task.
       </p>
     </motion.section>
   )
