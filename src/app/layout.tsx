1bc3dbcfa96672dd0a98d00000b20c58f4056ce5
Taylor Horwood <taylor.horwood@outlook.com>
2023-10-17 20:54:50 +0000

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,43 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+import '@/styles/globals.css';
+
+import Header from '@/components/Header';
+import { ThemeProvider } from '@/components/ThemeProvider';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: {
+    template: `%s | Taylor Horwood`,
+    default: 'Taylor Horwood',
+  },
+  description: 'Personal Portfolio',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang='en'>
+      <body
+        className={`${inter.className} width-full bg-primary text-primary antialiased`}
+      >
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='system'
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Header />
+          <div className='px-6 pb-24 pt-16 md:px-6 md:pb-44 md:pt-20'>
+            {children}
+          </div>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/components/ui/NavLink.tsx
@@ -0,0 +1,26 @@
+import cn from 'clsx';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { ReactNode } from 'react';
+
+type NavLinkProps = {
+  href: string;
+  children: ReactNode;
+};
+
+export default function NavLink({ href, children }: NavLinkProps) {
+  const pathname = `/${usePathname().split('/')[1]}`; // active paths on dynamic subpages
+  const active = pathname === href;
+
+  return (
+    <Link
+      className={cn(
+        'rounded-lg px-4 py-2 transition-colors hover:text-primary',
+        active ? 'bg-tertiary text-primary' : 'text-secondary',
+      )}
+      href={href}
+    >
+      {children}
+    </Link>
+  );
+}
