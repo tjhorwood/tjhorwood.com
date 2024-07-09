00b17672ef6cf2f5d4e35b32fa628f7f6c3a344c
Taylor Horwood <taylor.horwood@outlook.com>
2024-07-09 14:59:30 +0000

    updated packages

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -1,4 +1,4 @@
-import { Listbox } from '@headlessui/react';
+import { Listbox, ListboxButton, ListboxOption, ListboxOptions } from '@headlessui/react';
 import clsx from 'clsx';
 import { AnimatePresence, motion } from 'framer-motion';
 import { useTheme } from 'next-themes';
@@ -33,7 +33,7 @@ export default function ThemeSwitcher() {
           );
           return (
             <div className='relative'>
-              <Listbox.Button
+              <ListboxButton
                 className={clsx(
                   'relative flex h-8 w-8 cursor-default items-center justify-center rounded-full focus:outline-none ',
                 )}
@@ -43,20 +43,20 @@ export default function ThemeSwitcher() {
                 ) : (
                   <HiSun className={iconClassName} />
                 )}
-              </Listbox.Button>
+              </ListboxButton>
               <AnimatePresence>
                 {open && (
-                  <Listbox.Options
+                  <ListboxOptions
                     as={motion.ul}
                     static
                     initial={{ opacity: 0, scale: 0.9 }}
                     animate={{ opacity: 1, scale: 1 }}
                     exit={{ opacity: 0, scale: 0.9 }}
-                    transition={{ type: 'spring', bounce: 0.3, duration: 0.3 }}
+                    transition
                     className='absolute right-0 mt-2 max-h-60 w-36 origin-top-right space-y-1 overflow-auto rounded-xl bg-white p-2 text-base shadow-lg backdrop-blur-lg focus:outline-none dark:bg-black sm:text-sm'
                   >
                     {themes.map((theme) => (
-                      <Listbox.Option
+                      <ListboxOption
                         key={theme.id}
                         className={({ active, selected }) =>
                           clsx(
@@ -74,17 +74,16 @@ export default function ThemeSwitcher() {
                         {({ selected }) => (
                           <>
                             <span
-                              className={`relative w-full ${
-                                selected ? 'font-medium' : 'font-normal'
-                              }`}
+                              className={`relative w-full ${selected ? 'font-medium' : 'font-normal'
+                                }`}
                             >
                               {theme.label}
                             </span>
                           </>
                         )}
-                      </Listbox.Option>
+                      </ListboxOption>
                     ))}
-                  </Listbox.Options>
+                  </ListboxOptions>
                 )}
               </AnimatePresence>
             </div>
