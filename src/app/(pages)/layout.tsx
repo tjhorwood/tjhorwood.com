a186e793c050aaa76b61621d57f1d19e155c3f4a
Taylor Horwood <taylor.horwood@outlook.com>
2024-09-13 16:00:22 -0400

    updated hover functionality on the project page (#37)

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,23 +1,8 @@
-'use client';
-import AOS from 'aos';
-import { useEffect } from 'react';
-
-import 'aos/dist/aos.css';
-
 export default function DefaultLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  useEffect(() => {
-    AOS.init({
-      once: true,
-      disable: 'phone',
-      duration: 700,
-      easing: 'ease-out-cubic',
-    });
-  });
-
   return (
     <div className='px-6 pb-24 pt-10 md:px-6 md:pb-44 md:pt-16'>
       <main className='grow'>{children}</main>
