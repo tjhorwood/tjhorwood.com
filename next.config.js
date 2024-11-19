574163b6e568480a10cde4c7ba13751e6534b8a0
Taylor Horwood <taylor.horwood1@gmail.com>
2024-11-19 12:59:46 -0500

    updated to export config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   reactStrictMode: true,
+  output: 'export', // Ensures static HTML is generated
+  trailingSlash: true, // Optional: Adds trailing slashes to URLs for better Netlify compatibility
 };
 
 module.exports = nextConfig;
