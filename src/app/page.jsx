edf6993c0e90055a023625e907bc117e3cf26e3a
Taylor Horwood <taylor.horwood1@gmail.com>
2025-07-08 21:24:28 +0000

    updated structure

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,81 +3,79 @@ import Link from '@/components/Link';
 import { socialsData } from '@/lib/data';
 import Image from 'next/image';
 import { LuDownload, LuMail } from 'react-icons/lu';
+import AnimatedContent from '@/components/animations/AnimatedContent';
+import RotatingText from '@/components/animations/RotatingText';
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
-  const baseDelay = 0;
-  const delayIncrement = 100;
-  const aosAnimationType = 'fade-up';
-
   return (
     <div className='mx-auto flex max-w-4xl flex-col gap-16'>
       <div className='flex flex-col gap-8'>
-        <div
-          className='flex flex-col-reverse gap-6 text-neutral-900 md:flex-row md:items-center dark:text-white'
-          data-aos={aosAnimationType}
-        >
+        <div className='flex flex-col items-center gap-4 text-center md:flex-row md:gap-6 md:text-left'>
           <Image
             src={ProfilePic}
             alt='avatar'
-            width={150}
-            height={150}
-            className='rounded-full bg-neutral-200/50 dark:bg-neutral-800'
+            width={200}
+            height={200}
+            className='size-40 rounded-full bg-secondary' // Use fixed h/w for consistency
           />
-          <div className='ml-2 space-y-1'>
-            <h1 className='text-3xl font-bold tracking-tight text-neutral-900 dark:text-white'>
-              Taylor Horwood
-            </h1>
-            <p className='text-neutral-600 dark:text-neutral-400'>
-              Developer, tinkerer, indie hacker
-            </p>
-            <ul className='hidden space-x-6 pt-4 text-neutral-900 md:flex md:justify-start dark:text-white'>
-              {socialsData.map(({ name, href, icon: Icon }) => (
-                <li key={name}>
-                  <Link
-                    className='flex items-center gap-x-2 text-neutral-600 no-underline hover:text-neutral-900 dark:text-neutral-400 dark:hover:text-white'
-                    href={href}
-                  >
-                    <Icon className='h-6 w-6' />
-                    <span>{name}</span>
-                  </Link>
-                </li>
+          <div className='space-y-2'>
+            <div className='flex flex-col items-center gap-4 md:flex-row md:gap-4'>
+              <h1 className='text-3xl font-bold tracking-tight'>
+                Taylor Horwood
+              </h1>
+              <RotatingText
+                texts={['Developer', 'Engineer', 'Tinkerer', 'Indie Hacker']}
+                mainClassName='text-2xl px-3 bg-secondary overflow-hidden py-2 justify-center rounded-lg shadow'
+                staggerFrom={'last'}
+                initial={{ y: '100%' }}
+                animate={{ y: 0 }}
+                exit={{ y: '-120%' }}
+                staggerDuration={0.025}
+                splitLevelClassName='overflow-hidden'
+                transition={{ type: 'spring', damping: 30, stiffness: 400 }}
+                rotationInterval={2000}
+              />
+            </div>
+
+            {/* This list remains hidden on mobile as per your original code */}
+            <ul className='space-x-6 pt-6 md:pt-4 flex justify-center md:justify-start'>
+              {socialsData.map(({ name, href, icon: Icon }, index) => (
+                <AnimatedContent
+                  key={index}
+                  distance={50}
+                  direction='veritcal'
+                  reverse={false}
+                  duration={0.6}
+                  ease='power3.out'
+                  initialOpacity={0}
+                  animateOpacity
+                  scale={1}
+                  threshold={0}
+                  delay={index * 0.1}
+                >
+                  <li>
+                    <Link
+                      className='flex items-center gap-x-2 text-primary/60 no-underline hover:text-primary'
+                      href={href}
+                    >
+                      <Icon className='h-6 w-6' />
+                      <span>{name}</span>
+                    </Link>
+                  </li>
+                </AnimatedContent>
               ))}
             </ul>
           </div>
         </div>
-        <p
-          className='text-neutral-900 dark:text-white'
-          data-aos={aosAnimationType}
-          data-aos-delay={(baseDelay + 1 * delayIncrement).toString()}
-        >
-          Hello world, I&apos;m Taylor Horwood a site reliability engineer by
+        <p>
+          Hello world, my name is Taylor. I am a Site Reliability Engineer by
           day and full stack developer by night. I create flawless front-end
           experiences while taming DevOps challenges for seamless, dependable
           systems. Let&apos;s collaborate to transform your dream into digital
           magic, shaping a future where innovation meets unwavering reliability!
         </p>
-        <ul
-          className='flex max-w-full flex-wrap justify-start gap-6 py-2 md:hidden'
-          data-aos={aosAnimationType}
-          data-aos-delay={(baseDelay + 2 * delayIncrement).toString()}
-        >
-          {socialsData.map(({ name, href, icon: Icon }) => (
-            <li key={name}>
-              <Link
-                className='flex items-center gap-x-2 text-neutral-600 no-underline hover:text-neutral-900 dark:text-neutral-400 dark:hover:text-white'
-                href={href}
-              >
-                <Icon className='h-6 w-6' />
-                <span>{name}</span>
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <ul
-          className='flex flex-col gap-2 text-neutral-900 md:flex-row md:gap-6 dark:text-white'
-          data-aos={aosAnimationType}
-          data-aos-delay={(baseDelay + 3 * delayIncrement).toString()}
-        >
+        <div className='flex w-full flex-row space-x-2 sm:space-x-4 sm:w-auto'>
           {[
             {
               href: 'mailto:contact@tjhorwood.com',
@@ -86,20 +84,26 @@ export default function Home() {
             },
             { href: '/resume.pdf', text: 'Download resume', icon: LuDownload },
           ].map(({ href, text, icon: Icon }) => (
-            <li
+            <Link
               key={text}
-              className='text-neutral-600 transition-opacity hover:text-neutral-900 dark:text-neutral-400 dark:hover:text-white'
+              href={href}
+              className='flex-grow sm:flex-grow-0'
+              target='_blank'
+              rel='noopener noreferrer'
             >
-              <Link
-                href={href}
-                className='flex items-center gap-2 no-underline'
+              <Button
+                variant='secondary'
+                size='lg'
+                className='w-full cursor-pointer shadow'
               >
-                <Icon className='h-5 w-5' />
-                <span>{text}</span>
-              </Link>
-            </li>
+                <div className='flex items-center justify-center gap-2'>
+                  <Icon className='size-5' />
+                  <span>{text}</span>
+                </div>
+              </Button>
+            </Link>
           ))}
-        </ul>
+        </div>
       </div>
     </div>
   );
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -17,7 +17,7 @@ const WorkplaceItem = ({ item }) => {
   return (
     <Sheet>
       <SheetTrigger asChild>
-        <div className='flex w-full cursor-pointer items-center justify-between rounded-lg bg-neutral-200/50 p-4 text-left no-underline shadow-sm transition-all duration-200 hover:scale-[1.02] dark:bg-neutral-800'>
+        <div className='flex w-full cursor-pointer items-center justify-between rounded-lg bg-secondary p-4 text-left no-underline shadow-sm transition-all duration-200 hover:scale-[1.02]'>
           <div className='flex flex-1 items-center gap-4'>
             <Image
               src={item.imageSrc}
@@ -27,47 +27,29 @@ const WorkplaceItem = ({ item }) => {
               className='hidden rounded-full sm:block'
             />
             <div className='flex flex-col gap-px'>
-              <p className={`font-medium ${item.link ? 'external-arrow' : ''}`}>
-                {item.title}
-              </p>
-              <p className='text-sm text-neutral-700 dark:text-neutral-300'>
-                {item.company}
-              </p>
+              <p className='font-medium'>{item.title}</p>
+              <p className='text-primary/60'>{item.company}</p>
             </div>
           </div>
           {item.time && (
-            <p className='ml-3 text-right text-sm whitespace-nowrap text-neutral-700 dark:text-neutral-300'>
+            <p className='ml-3 text-right text-sm whitespace-nowrap text-primary'>
               {item.time}
             </p>
           )}
         </div>
       </SheetTrigger>
       <SheetContent
-        className={`border-none p-0 shadow-none [&>button[class*="absolute"][class*="right-4"][class*="top-4"]]:rounded-sm [&>button[class*="absolute"][class*="right-4"][class*="top-4"]]:opacity-70 [&>button[class*="absolute"][class*="right-4"][class*="top-4"]]:hover:opacity-100 [&>button[class*="absolute"][class*="right-4"][class*="top-4"]]:focus:opacity-100 [&>button[class*="absolute"][class*="right-4"][class*="top-4"]]:focus:ring-0 [&>button[class*="absolute"][class*="right-4"][class*="top-4"]]:focus:ring-offset-0`}
+        className={`min-w-11/12 sm:min-w-2/3 lg:min-w-1/2 xl:min-w-1/3 2xl:min-w-1/4 border-none [&>button[class*="absolute"][class*="right-4"][class*="top-4"]]:rounded-sm [&>button[class*="absolute"][class*="right-4"][class*="top-4"]]:opacity-70 [&>button[class*="absolute"][class*="right-4"][class*="top-4"]]:hover:opacity-100 [&>button[class*="absolute"][class*="right-4"][class*="top-4"]]:focus:opacity-100 [&>button[class*="absolute"][class*="right-4"][class*="top-4"]]:focus:ring-0 [&>button[class*="absolute"][class*="right-4"][class*="top-4"]]:focus:ring-offset-0`}
       >
-        <div
-          initial={{ x: '100%' }}
-          animate={{ x: 0 }}
-          transition={{
-            type: 'tween',
-            stiffness: 100,
-            damping: 20,
-            duration: 0.2,
-          }}
-          className='hide-scrollbar ml-auto h-full max-w-[95vw] overflow-y-auto rounded-l-xl bg-neutral-100 p-6 md:p-8 dark:bg-neutral-800'
-        >
-          <SheetHeader className='mb-4 text-left'>
+        <div className='hide-scrollbar ml-auto h-full w-full overflow-y-auto rounded-l-xl bg-secondary p-6 md:p-8'>
+          <SheetHeader className='text-left pl-0'>
             <SheetTitle className='text-2xl font-semibold'>
               {item.title}
             </SheetTitle>
-            <p className='text-md text-neutral-700 dark:text-neutral-300'>
-              {item.company}
-            </p>
+            <p className='text-md'>{item.company}</p>
           </SheetHeader>
-          <p className='py-2 text-sm text-neutral-700 md:text-base dark:text-neutral-300'>
-            Key responsibilities:
-          </p>
-          <ul className='ml-4 list-disc space-y-2 text-sm text-neutral-600 md:text-base dark:text-neutral-400'>
+          <p className='py-2 text-sm md:text-base'>Key responsibilities:</p>
+          <ul className='ml-4 list-disc space-y-2 text-sm md:text-base'>
             {item.description?.map((descItem, index) => (
               <li key={index}>{descItem.content}</li>
             ))}
@@ -78,17 +60,15 @@ const WorkplaceItem = ({ item }) => {
   );
 };
 
-export default function Work({ data_aos, data_aos_delay = '' }) {
+export default function Work() {
   const yearsOfExperience = useMemo(() => new Date().getFullYear() - 2011, []);
 
   return (
-    <div data-aos={data_aos} data-aos-delay={data_aos_delay}>
+    <div>
       <Section heading='Work' headingAlignment='left'>
         <div className='flex w-full flex-col space-y-6'>
-          <p className='text-neutral-700 dark:text-neutral-300'>
-            {yearsOfExperience}+ years of diverse professional experience.
-          </p>
-          <p className='text-neutral-700 dark:text-neutral-300'>
+          <p>{yearsOfExperience}+ years of diverse professional experience.</p>
+          <p>
             I began my career in customer facing roles, where I developed strong
             communication and interpersonal skills. I then transitioned to a
             technical support role, where I discovered my passion for
@@ -105,10 +85,10 @@ export default function Work({ data_aos, data_aos_delay = '' }) {
             ))}
           </ul>
 
-          <div>
+          <div className='w-full md:w-1/2 lg:w-1/3 xl:w-1/4'>
             <Button
               asChild
-              className='h-12 bg-neutral-900 text-base text-white transition-all hover:scale-[1.02] hover:bg-neutral-800 active:scale-[0.98] dark:bg-neutral-50 dark:text-gray-900 dark:hover:bg-neutral-100'
+              className='transition-all hover:scale-[1.02]'
               variant='default'
               size='lg'
             >
