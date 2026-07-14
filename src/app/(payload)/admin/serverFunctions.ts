2870ef4f612f552b857e4aca31afc0a5aa8f8c05
Taylor Horwood <tjhorwood@users.noreply.github.com>
2026-07-14 01:31:28 +0000

    Fix Payload admin server functions

--- src/app/(payload)/admin/serverFunctions.ts
+++ src/app/(payload)/admin/serverFunctions.ts
@@ -1,5 +1,13 @@
 'use server';
 
+import configPromise from '@payload-config';
 import { handleServerFunctions } from '@payloadcms/next/layouts';
 
-export const serverFunction = handleServerFunctions;
+import { importMap } from './importMap.js';
+
+export const serverFunction = async (args) =>
+  handleServerFunctions({
+    ...args,
+    config: configPromise,
+    importMap,
+  });
